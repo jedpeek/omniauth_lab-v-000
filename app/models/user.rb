906a361a44954e3236a70bd2f17247fdc2c14611
Jed Peek <jedpeek@yahoo.com>
2018-08-29 20:55:36 +0000

    automatically backed up by learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
 
   def self.find_or_create_by_omniauth(auth_hash)
-    oauth_email = auth_hash["info"]["email"]
+    oauth_uid = auth_hash["info"]["email"]
     oauth_name = auth_hash['info']['nickname']
     #first_or_create will always return an instance of a user
     where(uid: oauth_uid).first_or_create do |user|
