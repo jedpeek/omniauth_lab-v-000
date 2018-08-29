5332d778f41cde2d05a3af37a9e9d149e16ebd11
Jed Peek <jedpeek@yahoo.com>
2018-08-29 21:14:26 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,6 @@ class SessionsController < ApplicationController
     if auth_hash = request.env['omniauth.auth']
       user = User.find_or_create_by_omniauth(auth_hash)
       session[:user_id] = user.id
-      binding.pry
       return redirect_to controller:'sessions', action:'create'
     else
       render 'sessions/new'
