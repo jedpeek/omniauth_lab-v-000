93f7c7c67ecd17056f6f9d20f95b9ff15f63721e
Jed Peek <jedpeek@yahoo.com>
2018-08-29 21:21:42 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
 
   def create
       auth_hash = request.env['omniauth.auth']
-      user = User.find_or_create_by(uid: auth[:uid])
+      user = User.find_or_create_by(uid: auth_has[:uid])
         user.name = auth['info']['name']
         session[:user_id] = user.try(:id)
       return redirect_to controller:'sessions', action:'create'
