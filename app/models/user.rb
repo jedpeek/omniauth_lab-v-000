0c430b3df69773013eecd32dcf114a9f397bec2c
Jed Peek <jedpeek@yahoo.com>
2018-08-29 20:55:46 +0000

    automatically backed up by learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
     oauth_name = auth_hash['info']['nickname']
     #first_or_create will always return an instance of a user
     where(uid: oauth_uid).first_or_create do |user|
-      user.email = oauth_email
+      user.uid = oauth_email
       user.username = oauth_username
       user.password = SecureRandom.hex(10)
     end
